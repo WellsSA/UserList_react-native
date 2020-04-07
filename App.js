350dbe16106be3337b0df16cacc043c414a128a7
WellsSA <wel.cavzod@gmail.com>
2020-04-07 08:19:22 -0300

    App.js: code refactoring to manage users, not notes

--- App.js
+++ App.js
@@ -2,47 +2,53 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, TextInput, Button, View, FlatList } from 'react-native';
 
 export default function App() {
-  const [note, setNote] = useState('');
-  const [notes, setNotes] = useState([]);
-  const [noteCounter, setNoteCounter] = useState(0);
-
-  // handle user changes on input
-  const handleNoteInput = stickyNote => {
-    setNote(stickyNote);
-  };
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [users, setUsers] = useState([]);
+  const [userCounter, setUserCounter] = useState(0);
   
-  // adds note item to array with other sticky notes
-  const addNote = () => {
-    console.log (note);
+  // adds a user item to array with other users
+  const addUser = () => {
+    console.log (name);
     
-    setNotes (notes => {
-      setNoteCounter(noteCounter + 1);
-      return [...notes, {
-        key: String(noteCounter),
-        value: note,
+    setUsers (users => {
+      setUserCounter(userCounter + 1);
+      return [...users, {
+        key: String(userCounter * 10),
+        value: { name, phone },
       }];
     });
   }
 
   return (
     <View style={styles.mainView}>
-      <View style={styles.ReminderView}>
-      {/* usuário irá inserir lembretes aqui*/}
-      <TextInput 
-        placeholder="Lembrar..." 
-        style={styles.ReminderInput}
-        onChangeText={handleNoteInput}
-        value={note}
-      />
-      <Button title="+" onPress={addNote}></Button>
+      {/* Insert section */}
+      <View style={styles.inputView}>
+        <View style={styles.inputSection}>
+          <TextInput 
+            placeholder="Nome" 
+            style={styles.input}
+            onChangeText={setName}
+            value={name}
+          />
+        </View>
+        <View style={styles.inputSection}>
+          <TextInput 
+            placeholder="Telefone" 
+            style={styles.input}
+            onChangeText={setPhone}
+            value={phone}
+          />
+          <Button title="+" onPress={addUser}></Button>
+        </View>
       </View>
-      {/*Aqui será exibida a lista de lembretes*/}
+      {/* List seciton */}
       <FlatList
-        data={notes}
+        data={users}
         renderItem={
-          note => (
+          ({ item: { value: { name, phone } } }) => (
             <View style={styles.listItem}>
-              <Text>{note.item.value}</Text>
+              <Text>{name} - {phone}</Text>
             </View>
           )
         }
@@ -55,16 +61,24 @@ const styles = StyleSheet.create({
   mainView: {
     padding: 50,
   },
-  ReminderView: {
+  inputView: {
+    padding: 20,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  inputSection: {
+    width: '100%',
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
   },
-  ReminderInput: {
-    borderBottomColor: 'black',
-    borderBottomWidth: 1, 
+  input: {
+    borderLeftColor: 'black',
+    borderLeftWidth: 1, 
     marginBottom: 4, 
     padding: 2,
+    paddingLeft: 10,
     flex: 1,
   },
   listItem: {
